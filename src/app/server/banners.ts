c4a62c4776990e51fc00fac21b28dda9a7871ab0
muradevCode <muradkusherbaev@gmail.com>
2025-10-14 19:04:18 +0500

    sdf

--- src/app/server/banners.ts
+++ src/app/server/banners.ts
@@ -1,5 +1,6 @@
+import { httpClient } from "../httpClient/httpClient";
+
 export const getBanners = async () => {
-    const res = await fetch("https://back-texnoprom.uz/banners");
-    const data = await res.json();
-    return data;
+  const { data } = await httpClient.get("banners");
+  return data;
 };
